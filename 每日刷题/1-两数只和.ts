3c365832afe56f8e6c8b505e872e819c01fce355
anyahui <1549472324@qq.com>
2021-05-11 13:53:51 +0800

    每日一题

---
+++ 每日刷题/1-两数只和.ts
@@ -0,0 +1,26 @@
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+// 你可以按任意顺序返回答案。
+
+// 示例 1：
+
+// 输入：nums = [2,7,11,15], target = 9
+// 输出：[0,1]
+// 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+
+function twoSum(nums: number[], target: number): number[] {
+    let newArr: number[] = []
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                newArr[newArr.length] = i
+                newArr[newArr.length] = j
+            }
+        }
+    }
+    return newArr
+};
+
+console.log(twoSum([2,3,4,5,6,7,8,3,1], 6))
