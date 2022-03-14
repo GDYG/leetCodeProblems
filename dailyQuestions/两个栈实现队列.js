8f08048dace53f540b4870fcbb8c6ed109454cfc
anyahui <1549472324@qq.com>
2022-03-14 22:16:28 +0800

    feat: 牛客题3

---
+++ dailyQuestions/两个栈实现队列.js
@@ -0,0 +1,19 @@
+let stack1 = [], stack2 = [];
+
+function push(node)
+{
+    // write code here
+    stack1.push(node)
+}
+function pop()
+{
+    // write code here
+    if (stack2.length) {
+        return stack2.pop()
+    } else {
+        while (stack1.length) { 
+            stack2.push(stack1.pop())
+        }
+        return stack2.pop()
+    }
+}
---
+++ dailyQuestions/链表节点分K组旋转.js
@@ -0,0 +1,34 @@
+function reverseKGroup( head ,  k ) {
+    if (head == null || head.next == null) return head
+    let len = getLength(head)
+    if (len < k) {
+        return head
+    } else { 
+        let n1 = null, n2 = null;
+        for (let i = 1; i <= k; i++) { 
+            n1 = head.next;
+            head.next = n2;
+            n2 = head;
+            head = n1;
+        }
+        let lastNode = findLast(n2)
+        lastNode.next = reverseKGroup(head, k)
+        return lastNode
+    }
+    
+}
+function findLast(node) { 
+    let lastNode = node
+    while(lastNode.next) {
+        lastNode = lastNode.next
+    }
+    return lastNode
+}
+function getLength(node) {  
+    let len = 1;
+    while (node != null) { 
+        node = node.next
+        len += 1
+    }
+    return len
+}
